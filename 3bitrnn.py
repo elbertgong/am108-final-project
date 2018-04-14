59451ac3d2d2a9e89878d65275a735523a966832
elbertgong <elbertgong@college.harvard.edu>
2018-04-14 15:46:56 -0400

    more rnn outputs, and weight matrix

--- 3bitrnn.py
+++ 3bitrnn.py
@@ -97,9 +97,19 @@ torch.save(model.state_dict(), args.model_file)
 print("Model saved at", args.model_file)
 
 
-
-traj = Variable(torch.Tensor(torch.zeros((1000,3))), requires_grad=False)
+############### UTILS ################
+assert(0==1)
+traj = Variable(torch.Tensor(torch.zeros((40,3))), requires_grad=False)
 model = ThreeBitRNN(hidden_size=100)
 model.load_state_dict(torch.load('model.pkl'))
 no_inputs = model.all_hiddens(traj).data.numpy()
 np.savetxt("no_inputs.csv", no_inputs, delimiter=",")
+for i in range(2):
+    traj = Variable(torch.Tensor(torch.zeros((40,3))), requires_grad=False)
+    traj[20,i] = 1
+    out = model.all_hiddens(traj).data.numpy()
+    np.savetxt(str(i)+"perturb.csv", out, delimiter=",")
+
+# get weight matrix
+m = model.rnn.state_dict()['weight_hh_l0'].numpy()
+np.save('weight_hh_l0.npy', m)
